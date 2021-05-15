bae027a192803edb5f258b904d9926a50544e8a0
Samuele Vanini <samuele.vanini@live.it>
2021-05-15 10:53:37 +0200

    fix db and kafka

--- population_init.sql
+++ population_init.sql
@@ -1,2 +1,6 @@
-INSERT INTO public.tracked_point (code, point_name, description, location) VALUES (100, 'Piazza dei signori', 'Una delle piazze più 
-importati di padova', 'POINT(45.397959 11.87721)');
+INSERT INTO public.tracked_point (code, point_name, description, location) VALUES 
+(100, 'Paolotti', 'Luogo dove si consumano crimini disdicevoli', 'POINT(45.40811 11.88661)'), 
+(200, 'Prato della valle', 'Piazza europea grande grande', 'POINT(45.39922 11.87768)'), 
+(300, 'Piazza garibaldi', 'Piazza che ha visto morire la rinascente', 'POINT(45.40945 11.87766)'), 
+(400, 'Ali', 'Luogo dove i minorenni rubano alcol', 'POINT(45.38936 11.87766)'), 
+(500, 'Complesso fiera di Padova', 'Ci fanno il Pride in estate', 'POINT(45.41535 11.89066)');
